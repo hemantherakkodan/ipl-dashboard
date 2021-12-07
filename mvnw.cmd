98c6e9e35359f5fd3e84aa5baf5ed7046e1c2d6a
hemantherakkodan <61862963+hemantherakkodan@users.noreply.github.com>
2021-12-07 12:10:30 -0500

    Update mvnw.cmd
    Add echo message for learn

--- mvnw.cmd
+++ mvnw.cmd
@@ -61,7 +61,7 @@ set ERROR_CODE=0
 if not "%JAVA_HOME%" == "" goto OkJHome
 
 echo.
-echo Error: JAVA_HOME not found in your environment. >&2
+echo Error: JAVA_HOME not found in your environment.Check it! >&2
 echo Please set the JAVA_HOME variable in your environment to match the >&2
 echo location of your Java installation. >&2
 echo.
